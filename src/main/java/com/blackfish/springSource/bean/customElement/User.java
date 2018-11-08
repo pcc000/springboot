8cf08c55e777bbf624135455aadd4df3790fdc47
pengchengcheng <chengchengpeng@blackfish.com>
2018-11-08 14:01:40 +0800

    spring源码

---
+++ src/main/java/com/blackfish/springSource/bean/customElement/User.java
@@ -0,0 +1,29 @@
+package com.blackfish.springSource.bean.customElement;
+
+/**
+ * @Auther: chengchengpeng
+ * @Date: 2018/11/7 16:58
+ * @Description:
+ */
+public class User {
+
+    private String name;
+
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
