7fbb471792accf314a40801feaea19670108c1b4
shuyiwei <1097322117@qq.com>
2019-09-16 20:50:51 +0800

    装箱拆箱

---
+++ src/main/java/com/blackfish/java/BoxUtil.java
@@ -0,0 +1,21 @@
+package com.blackfish.java;
+
+/**
+ * @Auther: shuyiwei
+ * @Date: 2019/9/16 20:34
+ * @Description:
+ */
+public class BoxUtil {
+
+    public static void main(String[] args) {
+        Integer a =1;
+        Integer b =2;
+        Integer c =3;
+        Integer d =3;
+        Integer e =321;
+        Integer f =321;
+        Long g = 3L;
+        System.out.println(c==d);
+        System.out.println(e==f);
+    }
+}
--- src/main/java/com/blackfish/work/ShardingTest.java
+++ src/main/java/com/blackfish/work/ShardingTest.java
@@ -1,5 +1,9 @@
 package com.blackfish.work;
 
+import com.blackfish.java.util.common.JsonUtil;
+
+import java.util.*;
+
 /**
  * @Auther: SZSS
  * @Date: 2019/6/12 10:04
@@ -8,30 +12,56 @@ package com.blackfish.work;
 public class ShardingTest {
 
     public static void main(String[] args) {
-        long remember = Math.abs(Long.valueOf("18252059044").hashCode()) % 8;
-        System.out.println(remember);
+//        long remember = Math.abs(Long.valueOf("15960000001").hashCode()) % 8;
+//        System.out.println(remember);
+////
+//        long account = Math.abs("15960000001".hashCode()) % 8;
+//        System.out.println(account);
+//
+//        //shopNo
+//        int dataSource =("1000761".hashCode()%(2*32))/32;
+//        System.out.println(dataSource);
+//
+//        int table = ("1000761".hashCode()%(2*32))%32;
+//        System.out.println(table+NumericEnum.ONE.value);
+//
+//        int dataSource1 =("1000899".hashCode()%(2*32))/32;
+//        System.out.println(dataSource1);
+//
+//        int table1 = ("1000031".hashCode()%(2*32))%32;
+//        System.out.println(table1+NumericEnum.ONE.value);
 
-        long account = Math.abs("18252059044".hashCode()) % 8;
-        System.out.println(account);
+        List<String> list = new ArrayList<>();
+        list.add("1001054");
 
-        //shopNo
-        int dataSource =("1000761".hashCode()%(2*32))/32;
-        System.out.println(dataSource);
 
-        int table = ("1000761".hashCode()%(2*32))%32;
-        System.out.println(table+NumericEnum.ONE.value);
+        Set<String> stringSet = new HashSet<>();
 
-        int dataSource1 =("1000899".hashCode()%(2*32))/32;
-        System.out.println(dataSource1);
+        for(String str : list){
+            int sharding = Math.abs(str.hashCode()) % 32 + 1;
 
-        int table1 = ("1000031".hashCode()%(2*32))%32;
-        System.out.println(table1+NumericEnum.ONE.value);
+            int sub = Math.abs(str.hashCode()%64)/32;
+            String subss ="";
+            if (sub==0) {
+                subss=  "01";
+            }else if(sub==1){
+                subss=  "02";
+            }
+            String tablePrefix ="snt_stwms_sub_"+subss+".inventory_info_";
+            String s = String.valueOf(sharding);
+            if(s.length()==1){
+                s = "00"+s;
+            }else{
+                s = "0"+s;
+            }
+            stringSet.add(tablePrefix+s+" where shop_no ='"+str+"'");
+            System.out.println(tablePrefix+s+" where shop_no ='"+str+"'");
+        }
+//        System.out.println(JsonUtil.toJson(stringSet));
 
-        int sharding = Math.abs(("1000031").hashCode()) % 32 + 1;
-        System.out.println(sharding);
 
-        int sharding1 = Math.abs(("3148").hashCode()) % 2 + 1;
-        System.out.println(sharding1);
+//        int sharding1 = Math.abs(("1001165").hashCode()) % 2 + 1;
+//        System.out.println(sharding1);
 
     }
 }
