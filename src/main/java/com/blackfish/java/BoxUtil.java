76e980ba7dfa2298b51c8383305a0e3db06df1ca
shuyiwei <1097322117@qq.com>
2019-09-21 16:44:24 +0800

    装箱拆箱

--- src/main/java/com/blackfish/java/BoxUtil.java
+++ src/main/java/com/blackfish/java/BoxUtil.java
@@ -17,5 +17,9 @@ public class BoxUtil {
         Long g = 3L;
         System.out.println(c==d);
         System.out.println(e==f);
+        System.out.println(c==(a+b));
+        System.out.println(c.equals(a+b));
+        System.out.println(g == (a+b));
+        System.out.println(g.equals(a+b));
     }
 }
---
+++ src/main/java/com/blackfish/java/suanfa/BalloonNumTest.java
@@ -0,0 +1,13 @@
+package com.blackfish.java.suanfa;
+
+/**
+ * @Auther: shuyiwei
+ * @Date: 2019/9/21 16:43
+ * @Description:
+ * 给你一个字符串 text，你需要使用 text 中的字母来拼凑尽可能多的单词 "balloon"（气球）。
+ * 字符串 text 中的每个字母最多只能被使用一次。请你返回最多可以拼凑出多少个单词 "balloon"。
+ *
+ *
+ */
+public class BalloonNumTest {
+}
