3b65c8b96fbb88ac78a715fbe6be777816e41fd6
pengchengcheng <1097322115@qq.com>
2020-06-17 23:13:25 +0800

    redis

--- src/main/java/com/blackfish/java/suanfa/LongestConsecutive.java
+++ src/main/java/com/blackfish/java/suanfa/LongestConsecutive.java
@@ -10,6 +10,8 @@ import java.util.Set;
  */
 public class LongestConsecutive {
 
+    private String[] strs;
+
     /**
      * 给定一个未排序的整数数组，找出最长连续序列的长度。
      * 示例:
@@ -41,10 +43,53 @@ public class LongestConsecutive {
         return result;
     }
 
+
+    /**
+     * 编写一个函数来查找字符串数组中的最长公共前缀。
+     * 如果不存在公共前缀，返回空字符串 ""。
+     * 示例 1:
+     * 输入: ["flower","flow","flight"]
+     * 输出: "fl"
+     * 示例 2:
+     * 输入: ["dog","racecar","car"]
+     * 输出: ""
+     * 解释: 输入不存在公共前缀。
+     * 说明:
+     * 所有输入只包含小写字母 a-z 。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/longest-common-prefix
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+        this.strs = strs;
+        if(strs.length==0) return "";
+        return result(strs.length-1);
+    }
+
+    private String result(int j){
+        if(j==0) return strs[0];
+        String before = result(j-1);
+        String result ="";
+        for(int k=0;k<strs[j].length();k++){
+            if(k<before.length() && before.charAt(k)==strs[j].charAt(k)){
+                result = result + before.charAt(k);
+            }else{
+                break;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         LongestConsecutive longestConsecutive = new LongestConsecutive();
         int[] nums = new int[]{100, 4, 200, 1, 3, 2};
         System.out.println(longestConsecutive.longestConsecutive(nums));
+
+        String[] strs = new String[]{"","aa","aaa"};
+        System.out.println(longestConsecutive.longestCommonPrefix(strs));
+
     }
 
 }
--- src/main/java/com/blackfish/java/suanfa/thread/alternatePrint/Foo.java
+++ src/main/java/com/blackfish/java/suanfa/thread/alternatePrint/Foo.java
@@ -1,107 +1,109 @@
-package com.blackfish.java.suanfa.thread.alternatePrint;
-
-import sun.misc.Unsafe;
-
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.LockSupport;
-import java.util.concurrent.locks.ReentrantLock;
-
-/**
- * @Auther: shuyiwei
- * @Date: 2020/5/7 20:20
- * @Description:
- */
-public class Foo {
-
-     void one() { System.out.println("one"); }
-     void two() { System.out.println("two"); }
-     void three() { System.out.println("three");}
-
-
-    public Foo() {
-
-    }
-
-    public void first(Runnable printFirst) throws InterruptedException {
-
-        // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
-    }
-
-
-    public void second(Runnable printSecond) throws InterruptedException {
-
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
-    }
-
-    public void third(Runnable printThird) throws InterruptedException {
-
-        // printThird.run() outputs "third". Do not change or remove this line.
-        printThird.run();
-    }
-
-    public static void main(String[] args) {
-        Foo f = new Foo();
-
-        Lock lock = new ReentrantLock();
-        Condition c1 = lock.newCondition();
-        Condition c2 = lock.newCondition();
-        Condition c3 = lock.newCondition();
-
-        AtomicInteger first = new AtomicInteger(0);
-        AtomicInteger second = new AtomicInteger(0);
-
-        new Thread(()->{
-//            f.one();
-//            first.incrementAndGet();
-            try {
-                lock.lock();
-                f.one();
-                c2.signal();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }finally {
-                lock.unlock();
-            }
-        }).start();
-
-        new Thread(()->{
-//           while(first.get()!=1){
+//package com.blackfish.java.suanfa.thread.alternatePrint;
 //
-//           }
-//           f.two();
-//           second.incrementAndGet();
-            try {
-                lock.lock();
-                f.two();
-                c3.signal();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }finally {
-                lock.unlock();
-            }
-        }).start();
-
-
-        new Thread(()->{
-//            while (second.get()!=1){
+//import oracle.jvm.hotspot.jfr.ThreadStates;
+//import sun.misc.Unsafe;
 //
+//import java.util.concurrent.atomic.AtomicInteger;
+//import java.util.concurrent.locks.Condition;
+//import java.util.concurrent.locks.Lock;
+//import java.util.concurrent.locks.LockSupport;
+//import java.util.concurrent.locks.ReentrantLock;
+//
+///**
+// * @Auther: shuyiwei
+// * @Date: 2020/5/7 20:20
+// * @Description:
+// */
+//public class Foo {
+//
+//     void one() { System.out.println("one"); }
+//     void two() { System.out.println("two"); }
+//     void three() { System.out.println("three");}
+//
+//
+//    public Foo() {
+//
+//    }
+//
+//    public void first(Runnable printFirst) throws InterruptedException {
+//
+//        // printFirst.run() outputs "first". Do not change or remove this line.
+//        printFirst.run();
+//    }
+//
+//
+//    public void second(Runnable printSecond) throws InterruptedException {
+//
+//        // printSecond.run() outputs "second". Do not change or remove this line.
+//        printSecond.run();
+//    }
+//
+//    public void third(Runnable printThird) throws InterruptedException {
+//
+//        // printThird.run() outputs "third". Do not change or remove this line.
+//        printThird.run();
+//    }
+//
+//    public static void main(String[] args) {
+//        Foo f = new Foo();
+//
+//        Thread.State
+//        Lock lock = new ReentrantLock();
+//        Condition c1 = lock.newCondition();
+//        Condition c2 = lock.newCondition();
+//        Condition c3 = lock.newCondition();
+//
+//        AtomicInteger first = new AtomicInteger(0);
+//        AtomicInteger second = new AtomicInteger(0);
+//
+//        new Thread(()->{
+////            f.one();
+////            first.incrementAndGet();
+//            try {
+//                lock.lock();
+//                f.one();
+//                c2.signal();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }finally {
+//                lock.unlock();
 //            }
-//            f.three();
-            try {
-                lock.lock();
-                c3.await();
-                f.three();
-                c1.signal();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }finally {
-                lock.unlock();
-            }
-        }).start();
-    }
-
-}
+//        }).start();
+//
+//        new Thread(()->{
+////           while(first.get()!=1){
+////
+////           }
+////           f.two();
+////           second.incrementAndGet();
+//            try {
+//                lock.lock();
+//                f.two();
+//                c3.signal();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }finally {
+//                lock.unlock();
+//            }
+//        }).start();
+//
+//
+//        new Thread(()->{
+////            while (second.get()!=1){
+////
+////            }
+////            f.three();
+//            try {
+//                lock.lock();
+//                c3.await();
+//                f.three();
+//                c1.signal();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }finally {
+//                lock.unlock();
+//            }
+//        }).start();
+//    }
+//
+//}
