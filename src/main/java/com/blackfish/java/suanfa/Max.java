cecacd5f09894bf15e1594a322736620918eed73
pengchengcheng <1097322115@qq.com>
2020-06-20 23:12:30 +0800

    redis

---
+++ src/main/java/com/blackfish/java/suanfa/Max.java
@@ -0,0 +1,37 @@
+package com.blackfish.java.suanfa;
+
+public class Max {
+
+    /**
+     * 给定正整数数组 A，A[i] 表示第 i 个观光景点的评分，并且两个景点 i 和 j 之间的距离为 j - i。
+     * 一对景点（i < j）组成的观光组合的得分为（A[i] + A[j] + i - j）：景点的评分之和减去它们两者之间的距离。
+     * 返回一对观光景点能取得的最高分。
+     * 示例：
+     * 输入：[8,1,5,2,6]
+     * 输出：11
+     * 解释：i = 0, j = 2, A[i] + A[j] + i - j = 8 + 5 + 0 - 2 = 11
+     * 提示：
+     * 2 <= A.length <= 50000
+     * 1 <= A[i] <= 1000
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/best-sightseeing-pair
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param A
+     * @return
+     */
+    public int maxScoreSightseeingPair(int[] A) {
+        int result =0,mx = A[A.length-1]-(A.length-1);
+        for(int i=A.length-2;i>=0;i--){
+            result = result > mx+A[i]+i ? result : mx+A[i]+i;
+
+            mx = mx > A[i]-i ? mx : A[i]-i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] A = new int[]{10,4,6,4,10};
+        Max max = new Max();
+        System.out.println(max.maxScoreSightseeingPair(A));
+    }
+}
