239f4de5e169a43a33f5a8d80654e0b35ca9158d
shuyiwei <1097322117@qq.com>
2020-12-07 19:18:41 +0800

    Node

--- src/main/java/com/blackfish/java/suanfa/LinkList/Middle.java
+++ src/main/java/com/blackfish/java/suanfa/LinkList/Middle.java
@@ -367,6 +367,43 @@ public class Middle {
     }
 
 
+    /**
+     * 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+     * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+     * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+     * 示例 1:
+     * 给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+     * 示例 2:
+     * 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/reorder-list
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param head
+     */
+    public void reorderList(ListNode head) {
+        int count =0;
+        ListNode cur = head;
+        while(null != cur){
+            count ++;
+            cur = cur.next;
+        }
+
+        Stack<ListNode> reverse = new Stack<>();
+        count = count/2;
+        cur = head;
+        while(count!=0){
+            count --;
+            cur = cur.next;
+        }
+        while(null != cur){
+            reverse.add(cur);
+            cur = cur.next;
+        }
+        
+
+    }
+
+
 
     public static void printNode(Node head){
         StringBuffer result = new StringBuffer();
