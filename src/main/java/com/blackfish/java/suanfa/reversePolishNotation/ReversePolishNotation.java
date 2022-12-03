3a482acf5f0dbf88472eacb96952fa2a252c3db0
pcc <1097322115@qq.com>
2022-12-03 11:30:14 +0800

    11

--- src/main/java/com/blackfish/java/suanfa/reversePolishNotation/ReversePolishNotation.java
+++ src/main/java/com/blackfish/java/suanfa/reversePolishNotation/ReversePolishNotation.java
@@ -1,10 +1,10 @@
 package com.blackfish.java.suanfa.reversePolishNotation;
 
-import com.blackfish.java.util.common.JsonUtil;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class ReversePolishNotation {
 
@@ -17,20 +17,20 @@ public class ReversePolishNotation {
         operatorMap.put("*",20);
     }
 
-    public List<String> getSufix(String str){
-        List<String> result = new ArrayList<>();
+    /**
+     * 中缀表达式转后缀表达式
+     * @param str
+     * @return
+     */
+    public Queue<String> getSufix(String str){
+        Queue<String> resultQueue = new ArrayDeque<>();
         if(StringUtils.isEmpty(str)){
-            return result;
+            return resultQueue;
         }
         Stack<String> stack = new Stack<>();
         //处理多位数
         StringBuilder multiNum = new StringBuilder();
         for(int i=0;i<str.length();i++){
-            System.out.println("current str:"+String.valueOf(str.charAt(i)));
-            System.out.println("result str:"+JsonUtil.toJson(result));
-            System.out.println("stack :"+ JsonUtil.toJson(stack));
-            System.out.println("============================================");
-
             //如果是操作符
             if(str.charAt(i) <48 || str.charAt(i)> 57){
                 if(CollectionUtils.isEmpty(stack)){
@@ -43,7 +43,7 @@ public class ReversePolishNotation {
                     }
                     if(")".equals(String.valueOf(str.charAt(i)))){
                         while (!top.equals("(")){
-                            result.add(stack.pop());
+                            resultQueue.add(stack.pop());
                             top = stack.peek();
                         }
                         stack.pop();
@@ -54,7 +54,7 @@ public class ReversePolishNotation {
                     }else{
                         while (!stack.isEmpty() && !(operatorMap.get(String.valueOf(str.charAt(i))) > operatorMap.get(top))){
                             top = stack.pop();
-                            result.add(top);
+                            resultQueue.add(top);
                         }
                         stack.push(String.valueOf(str.charAt(i)));
                     }
@@ -67,17 +67,52 @@ public class ReversePolishNotation {
                 }
                 // 注意：这里要进行 i-- 操作退回上一个索引，不然外层for循环还会进行 i++ 的操作，这样会跳过表达式字符
                 i--;
-                result.add(multiNum.toString());
+                resultQueue.add(multiNum.toString());
             }
         }
         while (!stack.isEmpty()){
-            result.add(stack.pop());
+            resultQueue.add(stack.pop());
         }
-        return result;
+        return resultQueue;
+    }
+
+
+    public Integer caculate(Queue<String> suffixExpre){
+        if(CollectionUtils.isEmpty(suffixExpre)){
+            return 0;
+        }
+        Stack<String> numStack = new Stack<>();
+        while(!suffixExpre.isEmpty()){
+            String str = suffixExpre.poll();
+            if(operatorMap.keySet().contains(str)){
+                Integer num1 = Integer.valueOf(numStack.pop());
+                Integer num2 = Integer.valueOf(numStack.pop());
+                Integer res = 0;
+                switch (str){
+                    case "+":
+                        res = num2 + num1;
+                        break;
+                    case "-":
+                        res = num2 - num1;
+                        break;
+                    case "*":
+                        res = num2 * num1;
+                        break;
+                    case "/":
+                        res = num2 / num1;
+                        break;
+                }
+                numStack.push(res.toString());
+            }else{
+                numStack.push(str);
+            }
+        }
+        return Integer.valueOf(numStack.pop());
     }
 
     public static void main(String[] args) {
         ReversePolishNotation polish = new ReversePolishNotation();
         System.out.println(polish.getSufix("8+(6-1)*2+10/2"));
+        System.out.println(polish.caculate(polish.getSufix("8+(6-1)*2+10/2")));
     }
 }
